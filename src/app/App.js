f1f12dd4463f2e968a8a1ce90b3bd90c6f5f0399
bogdanm <bogdan.maier1@gmail.com>
2021-08-03 10:36:15 +0300

    Final touches (2)

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch, } from 'react-router-dom';
 import { ArticlesProvider } from '../context/articles/articles-context';
 import { LanguageProvider, LANGUAGES } from '../context/language/language-context';
 import AdminDashboard from '../pages/admin-dashboard/AdminDashboard';
@@ -15,7 +15,16 @@ function App() {
                     <div className="App">
                         <Container fluid="md">
                             <Row sm>
-                                <Col  md={{ span: 6, offset: 3 }}>
+                                <Col md={{ span: 6, offset: 3 }}>
+                                    <div className="nav">
+                                        <Link to={`/en/articles`}>
+                                            {`reader en articles`}
+                                        </Link>
+                                        <br/>
+                                        <Link to={`/admin/en/articles`}>
+                                            {`admin en articles`}
+                                        </Link>
+                                    </div>
                                     <Switch>
                                         <Route path="/admin">
                                             <AdminDashboard/>
